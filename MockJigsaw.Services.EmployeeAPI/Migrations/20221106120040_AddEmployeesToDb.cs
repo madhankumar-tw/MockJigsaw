d11d880ea929fbd891966ac8d036211c8e2d5344
Madhankumar <madhan.kumar@thoughtworks.com>
2022-11-06 18:17:44 +0530

    Create Dtos

---
+++ MockJigsaw.Services.EmployeeAPI/Migrations/20221106120040_AddEmployeesToDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MockJigsaw.Services.EmployeeAPI.Migrations
+{
+    public partial class AddEmployeesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    Office = table.Column<string>(type: "text", nullable: false),
+                    Experience = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ MockJigsaw.Services.EmployeeAPI/Models/Dto/EmployeeDto.cs
@@ -0,0 +1,10 @@
+namespace MockJigsaw.Services.EmployeeAPI.Models.Dto;
+
+public class EmployeeDto
+{
+    public int EmployeeId { get; set; }
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public string Office { get; set; }
+    public string Experience { get; set; }
+}
